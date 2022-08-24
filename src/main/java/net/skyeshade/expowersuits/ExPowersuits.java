38fc8dc22918aa2b35b54ed4a6b7b6eb5c4991de
SkyeShade <Zpectspace@gmail.com>
2022-08-24 23:59:44 +0200

    mod id change

--- src/main/java/net/skyeshade/expowersuits/ExPowersuits.java
+++ src/main/java/net/skyeshade/expowersuits/ExPowersuits.java
@@ -1,27 +1,20 @@
-package net.skyeshade.ex_powersuits;
+package net.skyeshade.expowersuits;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.skyeshade.expowersuits.item.ModItems;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExPowersuits.MOD_ID)
 public class ExPowersuits
 {
-    public static final String MOD_ID = "ex_powersuits";
+    public static final String MOD_ID = "expowersuits";
 
 
     // Directly reference a slf4j logger
@@ -31,7 +24,11 @@ public class ExPowersuits
     public ExPowersuits()
     {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
 
         // Register ourselves for server and other game events we are interested in
