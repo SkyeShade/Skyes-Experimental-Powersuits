a482afee545bd34b2f671f0d8aa4cb13d50c09f2
SkyeShade <Zpectspace@gmail.com>
2022-08-28 23:51:19 +0200

    Fixed Modid in mods.toml and added hydraulics items

--- src/main/java/net/skyeshade/expowersuits/item/ModItems.java
+++ src/main/java/net/skyeshade/expowersuits/item/ModItems.java
@@ -13,10 +13,17 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, ExPowersuits.MOD_ID);
 
 
-    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+    public static final RegistryObject<Item> LIGHT_HYDRAULIC = ITEMS.register("light_hydraulic",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> MEDIUM_HYDRAULIC = ITEMS.register("medium_hydraulic",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> HEAVY_HYDRAULIC = ITEMS.register("heavy_hydraulic",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ULTRAHEAVY_HYDRAULIC = ITEMS.register("ultraheavy_hydraulic",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 
     public static void register(IEventBus eventBus) {
