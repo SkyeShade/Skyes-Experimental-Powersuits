39125a1a5cc3c3fb0a07ae9ab8f4f6859f644822
SkyeShade <Zpectspace@gmail.com>
2022-08-29 15:06:16 +0200

    added titanium ore block variants

--- src/main/java/net/skyeshade/expowersuits/ExPowersuits.java
+++ src/main/java/net/skyeshade/expowersuits/ExPowersuits.java
@@ -7,6 +7,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.skyeshade.expowersuits.block.ModBlocks;
 import net.skyeshade.expowersuits.item.ModItems;
 import org.slf4j.Logger;
 
@@ -27,6 +28,7 @@ public class ExPowersuits
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
---
+++ src/main/java/net/skyeshade/expowersuits/block/ModBlocks.java
@@ -0,0 +1,48 @@
+package net.skyeshade.expowersuits.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.skyeshade.expowersuits.ExPowersuits;
+import net.skyeshade.expowersuits.item.ModCreativeModeTab;
+import net.skyeshade.expowersuits.item.ModItems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, ExPowersuits.MOD_ID);
+
+    // add blocks here
+    public static final RegistryObject<Block> TITANIUM_ORE = registerBlock("titanium_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops().explosionResistance(1200f)), ModCreativeModeTab.EXPERIMENTAL_POWERSUITS_TAB);
+
+    public static final RegistryObject<Block> DEEP_TITANIUM_ORE = registerBlock("deep_titanium_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.DEEPSLATE).strength(6f).requiresCorrectToolForDrops().explosionResistance(1200f)), ModCreativeModeTab.EXPERIMENTAL_POWERSUITS_TAB);
+
+
+    //register stuff that i dont understand 100%
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab)  {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                           CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
